8ae1844c65fcc557f664dde38bd107f7f9b02e78
Junhwi Kim <junhwi.kim23@gmail.com>
2016-09-06 17:47:30 +0900

    Reformat

--- slack_logger/__init__.py
+++ slack_logger/__init__.py
@@ -5,58 +5,56 @@ from logging.handlers import HTTPHandler
 
 
 class SlackHandler(HTTPHandler):
+    def __init__(self, url, username=None, icon_url=None, icon_emoji=None, channel=None):
+        o = urlparse(url)
+        is_secure = o.scheme == 'https'
+        HTTPHandler.__init__(self, o.netloc, o.path, method="POST", secure=is_secure)
+        self.username = username
+        self.icon_url = icon_url
+        self.icon_emoji = icon_emoji
+        self.channel = channel
 
-  def __init__(self, url, username=None, icon_url=None, icon_emoji=None, channel=None):
-    o = urlparse(url)
-    is_secure = o.scheme == 'https'
-    HTTPHandler.__init__(self, o.netloc, o.path, method="POST", secure=is_secure)
-    self.username = username
-    self.icon_url = icon_url
-    self.icon_emoji = icon_emoji
-    self.channel = channel
+    def mapLogRecord(self, record):
+        if isinstance(self.formatter, SlackFormatter):
+            payload = {
+                'attachments': [
+                    self.format(record),
+                ],
+            }
+        else:
+            payload = {
+                'text': self.format(record),
+            }
 
-  def mapLogRecord(self, record):
-    if isinstance(self.formatter, SlackFormatter):
-      payload = {
-        'attachments': [
-          self.format(record),
-        ],
-      }
-    else:
-      payload = {
-        'text': self.format(record),
-      }
+        if self.username:
+            payload['username'] = self.username
+        if self.icon_url:
+            payload['icon_url'] = self.icon_url
+        if self.icon_emoji:
+            payload['icon_emoji'] = self.icon_emoji
+        if self.channel:
+            payload['channel'] = self.channel
 
-    if self.username:
-      payload['username'] = self.username
-    if self.icon_url:
-      payload['icon_url'] = self.icon_url
-    if self.icon_emoji:
-      payload['icon_emoji'] = self.icon_emoji
-    if self.channel:
-      payload['channel'] = self.channel
-
-    ret = {
-      'payload': json.dumps(payload),
-    }
-    return ret
+        ret = {
+            'payload': json.dumps(payload),
+        }
+        return ret
 
 
 class SlackFormatter(logging.Formatter):
+    def format(self, record):
+        ret = {}
+        if record.levelname == 'INFO':
+            ret['color'] = 'good'
+        elif record.levelname == 'WARNING':
+            ret['color'] = 'warning'
+        elif record.levelname == 'ERROR':
+            ret['color'] = '#E91E63'
+        elif record.levelname == 'CRITICAL':
+            ret['color'] = 'danger'
 
-  def format(self, record):
-    ret = {}
-    if record.levelname == 'INFO':
-      ret['color'] = 'good'
-    elif record.levelname == 'WARNING':
-      ret['color'] = 'warning'
-    elif record.levelname == 'ERROR':
-      ret['color'] = '#E91E63'
-    elif record.levelname == 'CRITICAL':
-      ret['color'] = 'danger'
-
-    ret['author_name'] = record.levelname
-    ret['title'] = record.name
-    ret['ts'] = record.created
-    ret['text'] = super(SlackFormatter, self).format(record)
-    return ret
+        ret['author_name'] = record.levelname
+        ret['title'] = record.name
+        ret['ts'] = record.created
+        ret['text'] = super(SlackFormatter, self).format(record)
+        return ret
