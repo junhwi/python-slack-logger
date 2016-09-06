39cb74a82b4cf34058666401babe5557a4af7f03
Junhwi Kim <junhwi.kim23@gmail.com>
2016-09-06 17:44:56 +0900

    Add formatter

--- slack_logger/__init__.py
+++ slack_logger/__init__.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from urllib.parse import urlparse
 from logging.handlers import HTTPHandler
 
@@ -15,9 +16,17 @@ class SlackHandler(HTTPHandler):
     self.channel = channel
 
   def mapLogRecord(self, record):
-    payload = {
-      'text': self.format(record),
-    }
+    if isinstance(self.formatter, SlackFormatter):
+      payload = {
+        'attachments': [
+          self.format(record),
+        ],
+      }
+    else:
+      payload = {
+        'text': self.format(record),
+      }
+
     if self.username:
       payload['username'] = self.username
     if self.icon_url:
@@ -30,4 +39,24 @@ class SlackHandler(HTTPHandler):
     ret = {
       'payload': json.dumps(payload),
     }
+    return ret
+
+
+class SlackFormatter(logging.Formatter):
+
+  def format(self, record):
+    ret = {}
+    if record.levelname == 'INFO':
+      ret['color'] = 'good'
+    elif record.levelname == 'WARNING':
+      ret['color'] = 'warning'
+    elif record.levelname == 'ERROR':
+      ret['color'] = '#E91E63'
+    elif record.levelname == 'CRITICAL':
+      ret['color'] = 'danger'
+
+    ret['author_name'] = record.levelname
+    ret['title'] = record.name
+    ret['ts'] = record.created
+    ret['text'] = super(SlackFormatter, self).format(record)
     return ret
